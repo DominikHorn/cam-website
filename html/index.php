817dcdbefbce7d1b7d68b5620dd7287cb7ea80bb
Dominik Horn <horn.dominik101@yahoo.de>
2016-09-06 21:22:07 +0200

    Simple Form

--- html/index.php
+++ html/index.php
@@ -1,21 +1,33 @@
+<?php
+/*  include '../php/browserUtil.php';
+  
+  $ua = getBrowserInfo();
+  $yourbrowser = "Your browser: " . $ua['name'] . " " . $ua['version'] . 
+  " on " .$ua['platform'] . " reports: <br >" . $ua['userAgent'];
+
+  echo $yourbrowser;*/
+  if( $_POST["name"] || $_POST["age"] ) {
+    if (preg_match("/[^A-Za-z'-]/",$_POST['name'] )) {
+      die ("invalid name and name should be alpha");
+    }
+      
+    echo "Welcome ". $_POST['name']. "<br />";
+    echo "You are ". $_POST['age']. " years old.";
+      
+    exit();
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Cosmetics and More</title>
 </head>
 <body>
-	<div>
-    	<?php
-      		include '../php/browserUtil.php';
-
-		    $ua = getBrowserInfo();
-   			$yourbrowser = "Your browser: " . $ua['name'] . " " . $ua['version'] . 
-   			" on " .$ua['platform'] . " reports: <br >" . $ua['userAgent'];
-   
-        // Dies ist ein test
-
-   			echo $yourbrowser;
-      	?>
-   </div>
+  <form action = "<?php $_PHP_SELF ?>" method = "POST">
+    Name: <input type = "text" name = "name" />
+    Age: <input type = "text" name = "age" />
+    <input type = "submit" />
+  </form>
 </body>
 </html>
